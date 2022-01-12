52a56e72a1ebf253f40ad80bdf61421744e2b1b9
studentclass335428 <studentclass335428@gmail.com>
2022-01-12 13:54:50 +0700

    Hari kedua

--- main.js
+++ main.js
@@ -1,2 +1,33 @@
-const myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello World!';
+let percobaan1 = "makan di";
+let percobaan2 = "rumah makan";
+let percobaan3 = "kampanye";
+percobaan1 = "belajar";
+percobaan2 = "di";
+percobaan3 = "praxis academy";
+console.log(percobaan1,percobaan2,percobaan3);
+
+var namaHewan = "kucing";
+var jenisKelamin = "perempuan";
+var umur = "5 tahun";
+console.log(namaHewan,jenisKelamin,umur);
+
+let umurAndy = 2 ;
+let umurYudi = 4;
+let umurHasan = 6;
+let total = umurAndy*umurYudi*umurHasan
+let total1 = umurAndy-umurYudi-umurHasan
+console.log(total);
+
+let a = 1;
+let b = 2;
+let c = a+b;
+let d = a-b;
+let e = a*b;
+let f = a**b;
+let g = a/b;
+let h = a%b;
+console.log(c,d,e,f,g,h);
+
+// console.log(10);
+
+
