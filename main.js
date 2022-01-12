05479650706e83997e50f79a8570e46c08b0a9c5
studentclass335428 <studentclass335428@gmail.com>
2022-01-12 17:36:22 +0700

    Baru2

--- main.js
+++ main.js
@@ -4,19 +4,19 @@ let percobaan3 = "kampanye";
 percobaan1 = "belajar";
 percobaan2 = "di";
 percobaan3 = "praxis academy";
-console.log(percobaan1,percobaan2,percobaan3);
+// console.log(percobaan1,percobaan2,percobaan3);
 
 var namaHewan = "kucing";
 var jenisKelamin = "perempuan";
 var umur = "5 tahun";
-console.log(namaHewan,jenisKelamin,umur);
+// console.log(namaHewan,jenisKelamin,umur);
 
 let umurAndy = 2 ;
 let umurYudi = 4;
 let umurHasan = 6;
 let total = umurAndy*umurYudi*umurHasan
 let total1 = umurAndy-umurYudi-umurHasan
-console.log(total);
+// console.log(total);
 
 let a = 1;
 let b = 2;
@@ -26,8 +26,77 @@ let e = a*b;
 let f = a**b;
 let g = a/b;
 let h = a%b;
-console.log(c,d,e,f,g,h);
+// console.log(c,d,e,f,g,h);
 
 // console.log(10);
+
+let nama1 = "aditya"
+let nama2 = "achmad"
+let nama3 = nama1 + nama2
+// console.log(nama3);
+
+var x = 9;
+x += 1;
+// console.log(x);
+
+var uangRobby = 5000
+uangRobby += 1000
+// console.log(uangRobby);
+
+var percobaan0 = "Selamat Siang ";
+percobaan0 += "adik adik";
+// console.log(percobaan0);
+
+var test1 = 10 + 10;
+var test2 = "10" + 10;
+var apple2 = "apple" + 10;
+// console.log(test1,test2,apple2);
+
+let o = 5 + 5;
+let y = 5 + 5;
+let z = "Hello" + 5;
+// console.log(o,y,z);
+
+let m = 4;
+let n = 7;
+let p = m + n;
+let q = m - n;
+let r = m * n;
+let s = m / n;
+let t = m % n;
+let u = (m + n) * 3;
+// console.log(u);
+
+// if (n < m) {
+//     console.log("mega");
+// } else {
+//     console.log("mini");
+// }
+
+const namaKades = "Sugiono";
+const namaCamat = "Agus";
+const jabatan = "KADES";
+
+// if (jabatan != "KADES") {
+//     console.log(namaKades);
+// } else {
+//     console.log(namaCamat);
+// }
+
+const apakahBoss = "True";
+const apakahMiniBoss = "False";
+
+if (!apakahBoss == !apakahMiniBoss) {
+    console.log("TRUE");
+    } else {
+    console.log("FALSE");
+}
+
+
+
+
+
+
+
 
 
